bc0aed51fe221ab7cf4d6339b91420c8cbea0983
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-09-08 19:56:31 +0200

    finish lab8 backend

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const axios = require('axios');
 const server = express(); // initialize your express app instance
 
 const weatherData = require('./data/weather.json');
 const cors = require('cors');
 server.use(cors());
 const PORT = process.env.PORT;
-
-
 
 class Movie {
   constructor(item) {
@@ -20,12 +19,16 @@ class Movie {
   }
 }
 
+// http://localhost:3002/movies
+// https://api.themoviedb.org/3/search/movie?api_key=1742e55e6961c331f1b0e9a8c7b098f1&query=amman
+
 server.get('/movies', (req, res) => {
   let searchQuery = req.query.city;
   console.log(searchQuery);
   const movie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
   axios.get(movie).then(movieValue => {
-    console.log(movieValue.data.results[0].title);
+    // console.log(movieValue.data.results[0].title);
+    console.log('backend data',movieValue.data.results)
 
     let movieInfo = movieValue.data.results.map((item) => {
       return new Movie(item);
@@ -33,48 +36,53 @@ server.get('/movies', (req, res) => {
     
     res.send(movieInfo);
   })
+
+  
     .catch(err => {
-      res.send(err.message);
+      res.send('Error!');
     });
 
 });
 
 
 
+
+
+
+
 class Forecast {
-  constructor(date,description){
-    this.date =date;
-    this.description = description;
+  constructor(item){
+    this.date =item.valid_date;;
+    this.description= item.weather.description;
   }
 }
 
-// http://localhost:3500/weather?searchQuery=Amman
-server.get('/weather',(req,res)=>{
+server.get('/weather', async (req, res) => {
+    const  latitude  = req.query.lat;
+    const longitude  = req.query.long;
 
-    let searchQuery = req.query.searchQuery;
-    let lat = req.query.latitude;
-    let lon = req.query.longitudinal;
-    const weather =  weatherData.find((item) => {
-      if (item.city_name === searchQuery || item.lon === lon || item.lat === lat) {
-        return(item);
-      }
-  
-    });
-    try {
-      const weatherDataArr = weather.data.map(item => {
-        let date = item.valid_date;
-        let description = `${item.weather.description}`;
-        return new Forecast(date,description);
-      });
-      res.send(weatherDataArr);
-      
-    }
-    catch(error) {
-      res.send('Error! Please enter a valid city');
-      console.log(weatherData);
-    }
-  
+// http://localhost:3030/weather?query=31?longitude=30
+// http://api.weatherbit.io/v2.0/forecast/daily?key=e2c95883c34745f58ae63470e722f634&lat=30&lon=32;
+const weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${latitude}&lon=${longitude}`;
+axios.get(weatherURL).then(fullWeatherData => {
+
+  console.log(fullWeatherData);
+
+  let wantedData = fullWeatherData.data.data.map((item) => {
+    return new Forecast(item);
   });
+//   console.log(wantedData);
+  
+  res.send(wantedData);
+})
+  .catch(err => {
+    res.send('err.message');
+    // console.log('sssdsdfsdf');
+  });
+
+});
+
+
   
   
   
